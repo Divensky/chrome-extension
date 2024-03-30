214f1b2d88b8ff92f5dbff6fdeb79d6717132fe5
Irina Kats <irina8kats@gmail.com>
2024-03-29 22:14:24 -0400

    refactor content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,17 +1,44 @@
-const article = document.querySelector('article');
-
-if (article) {
-  const text = article.textContent;
+function calculateReadingTime(element) {
+  const text = element.textContent;
   const wordMatchRegExp = /[^\s]+/g;
   const words = text.matchAll(wordMatchRegExp);
   const wordCount = [...words].length;
   const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement('p');
-  badge.classList.add('color-secondary-text', 'type--caption');
-  badge.textContent = `⏱️ ${readingTime} min read`;
+  return readingTime;
+}
 
-  const heading = article.querySelector('h1');
-  const date = article.querySelector('time')?.parentNode;
+function createBadge(readingTime) {
+  const badge = document.createElement('span');
+  badge.textContent = ` ⏱️ ${readingTime} min read`;
+  return badge;
+}
 
-  (date ?? heading).insertAdjacentElement('afterEnd', badge);
+function insertBadge(element, badge) {
+  const heading = element.querySelector('h1');
+  if (heading) {
+    heading.insertAdjacentElement('afterEnd', badge);
+  } else {
+    console.log('READING-TIME: No h1 found');
+  }
+}
+
+const element = document.querySelector('main');
+
+if (element) {
+  let readingTime = calculateReadingTime(element);
+  console.log('READING-TIME: calculated value ', readingTime);
+  if (readingTime === 0) {
+    console.log('READING-TIME: Did not find any text, retrying...');
+    setTimeout(() => {
+      readingTime = calculateReadingTime(element);
+      console.log('READING-TIME: After retrying got', readingTime);
+      const badge = createBadge(readingTime);
+      insertBadge(element, badge);
+    }, 3000);
+  } else {
+    const badge = createBadge(readingTime);
+    insertBadge(element, badge);
+  }
+} else {
+  console.log('READING-TIME: No element found');
 }
